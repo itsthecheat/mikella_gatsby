1993677ecccfd7d6807557717f991bba63fa3a81
Leslie Behum <llaurenb@gmail.com>
2022-09-05 16:56:31 -0400

    small changes

--- prismic-config.js
+++ prismic-config.js
@@ -2,7 +2,5 @@
 module.exports = {
   prismicRepo: 'mikellamillen',
   // The default language for content in your Prismic repository.
-  defaultLanguage: 'en-us',
-  // All available languages for content in your Prismic repository.
-  langs: ['en-us', 'fr-fr'],
+  defaultLanguage: 'en-us'
 }
--- src/pages/preview.js
+++ src/pages/preview.js
@@ -2,15 +2,9 @@ import * as React from 'react'
 import { withPrismicPreviewResolver } from 'gatsby-plugin-prismic-previews'
 import { linkResolver } from '../utils/linkResolver'
 
-const PreviewPage = () => (
-  <div>
-    <h1>Loading preview…</h1>
-  </div>
-)
+const PreviewPage = ({ isPreview, ...props }) => { 
+  if (isPreview === false) return 'Not a preview!'
+  return <div><h1>Loading preview…</h1></div>
+}
 
-export default withPrismicPreviewResolver(PreviewPage, [
-  {
-    repositoryName: 'mikellamillen',
-    linkResolver,
-  },
-])
+export default withPrismicPreviewResolver(PreviewPage)
