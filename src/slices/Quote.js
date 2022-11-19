145990c1db6a0c8c67668e0a25b3a0bbc530ab66
Leslie Behum <llaurenb@gmail.com>
2022-11-19 14:15:15 -0500

    fixed quotes

--- src/slices/Quote.js
+++ src/slices/Quote.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import { Col, Row, Container } from 'react-bootstrap'
 import * as styles from './Quote.module.css'
-
+import {ImQuotesLeft, ImQuotesRight} from 'react-icons/im'
 const Quote = ({ data }) => {
   
   return (  
-    <Container fluid>
+    <Container fluid  className={styles.quote}>
       <Row>
-      <Col>
-        <hr/>
-        <div dangerouslySetInnerHTML={{ __html: data.primary.quote.html }} /> 
-        <hr/>
+        <Col>
+           <hr />
+          <div className={styles.display}>
+             
+            <ImQuotesLeft className={styles.icons} />
+            <div dangerouslySetInnerHTML={{ __html: data.primary.quote.html }} /> 
+          <ImQuotesRight className={styles.icons}/>
+     
+          </div>
+           <hr/>
+          
         </Col>
       </Row>
     </Container>
